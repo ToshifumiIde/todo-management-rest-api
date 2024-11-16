f366709632aff8705c469d07c3028a434d837093
井手俊文 <toshifumiide198765@gmail.com>
2024-11-16 10:58:46 +0900

    #10 edit MessageKey

--- src/main/java/net/ti/todo/common/MessageKey.java
+++ src/main/java/net/ti/todo/common/MessageKey.java
@@ -1,5 +1,36 @@
 package net.ti.todo.common;
 
-public enum MessageKey {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
+
+public enum MessageKey {
+  
+  RESOURCE_NOT_FOUND("Resource Not Found");
+
+  private String value;
+
+  MessageKey(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static MessageKey fromValue(String value) {
+    for (MessageKey b : MessageKey.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
 }
